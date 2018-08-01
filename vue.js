671e48e09f3b39bcf5c3354980d9b227654ece6f
bugaryu <boogalooryu@gmail.com>
2018-08-01 18:02:12 +0900

    ページネート構築

--- vue.js
+++ vue.js
@@ -1,27 +1,32 @@
-new Vue({
+var pagenate = new Vue({
 	el: '#pagenate',
 	data: {
-		id: 0,
-		onStyle: { 
-			'background-color': '#C65248'
-		},
-		offStyle: {
-			'background-color': '#B5B4B5'
-		}
+		current: 0,
+		images: [ "./zafu_photos/top-no1.png",
+				"./zafu_photos/top-no2&product.png",
+				"./zafu_photos/top-no3.png",
+				],
+		count: 0,
+		timer: null
+	},
+	created: function () {
+		this.timer = setTimeout(this.nextPage, 3000);
+		this.count = this.images.length
 	},
 	methods: {
-		change(event) {
-			console.log(`this id: ${this.id}`);
-			console.log(`target: ${event.target.value}`);
-			this.id = event.target.value;
+		change: function (event) {
+			console.log(`this id: ${this.current}, target id: ${event.target.value}`);
+			this.current = event.target.value;
 		},
-		setStyle(id) {
-			if (this.id == id) {
-				return this.onStyle;	
-			} else {
-				return this.offStyle;
-			}
+		nextPage: function () {
+			this.current = (this.current + 1) % this.count;
+			console.log(`go to next Page. page id: ${this.current}`);
+		},
+	},
+	watch: {
+		current: function () {
+			clearTimeout(this.timer);
+			this.timer = setTimeout(this.nextPage, 3000);
 		}
 	}
 });
-
