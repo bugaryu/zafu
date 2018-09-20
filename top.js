f5ee6c0f55adeec4b76a884ed4a75e5a3ab4c79b
yofune <hal@soutaisei.tokyo>
2018-09-21 04:14:30 +0900

    完成版

--- top.js
+++ top.js
@@ -10,9 +10,9 @@ let pagenate = new Vue({
 	el: '#top',
 	data: {
 		currentPage: 0,
-		images: [ "./images/top1.png",
-				  "./images/top2.png",
-				  "./images/top3.png"
+		images: [ "./images/top1.jpg",
+				  "./images/top2.jpg",
+				  "./images/top3.jpg"
 				],
 		links: [ "#profile",
 				"#pdcWrapper",
@@ -43,31 +43,41 @@ let pagenate = new Vue({
 });
 
 let item1 = { 
-	link: "#about",
+	link: "topics01.html",
 	image: "./images/zf_1.png",
-	date: "2018.5.25",
+	date: "2018.9.25",
 	category: "お知らせイベント案内",
-	title: "禅ボディワーク開催のご案内",
-	schedule: "6 月 17 日(土)14:30~16:30",
-	text: "この文章はzafuホームページのためのダミーテキストです。ここにはzafuや禅、マインドフルネスについての文章が入ります。この文章はzafuホームページのためのダミーテキストです。ここにはzafuや禅、マインドフルネ",
+	title: "坐蒲「zafuざふ」効果がテレビで紹介されました！",
+	schedule: "「名医とつながる！たけしの家庭の医学」",
+	text: "禅寺の住職がきれいな姿勢を保つのは坐蒲による効果が大きいとテレビで紹介！《「zafuざふ」は、坐禅時に使用するだけではありません。空気量を調整することで、テレビで紹介された「巻きタオル簡易坐蒲」に",
 };
 let item2 = {
-	link: "#program",
+	link: "https://www.facebook.com/kokoro.karada.taiso/",
 	image: "./images/ssd_1.png",
-	date: "2018.5.25",
+	date: "2018.9.25",
 	category: "お知らせイベント案内",
-	title: "禅ボディワーク開催のご案内",
-	schedule: "6 月 17 日(土)14:30~16:30",
-	text: "この文章はzafuホームページのためのダミーテキストです。ここにはzafuや禅、マインドフルネスについての文章が入ります。この文章はzafuホームページのためのダミーテキストです。ここにはzafuや禅、マインドフルネ"
+	title: "「禅に学ぶ　こころが安らかになり",
+	schedule: "からだが緩み健康を築く　日々の瞑想会」 at 安詳寺",
+	text: "毎月、東京都大田区の「日蓮宗　安詳寺」にて瞑想会を開催しております。お気軽にお立ち寄りください。瞑想をして、日々の疲れと元気を回復しストレス発散いたしましょう！詳細はこちらにて。"
 };
 let item3 = {
-	link: "#dialogue",	
+	link: "http://bosan32000.wixsite.com/sinsindo/blank",	
 	image: "./images/zen.png",
-	date: "2018.5.25",
+	date: "2018.9.25",
 	category: "お知らせイベント案内",
-	title: "禅ボディワーク開催のご案内",
-	schedule: "6 月 17 日(土)14:30~16:30",
-	text: "この文章はzafuホームページのためのダミーテキストです。ここにはzafuや禅、マインドフルネスについての文章が入ります。この文章はzafuホームページのためのダミーテキストです。ここにはzafuや禅、マインドフルネ"
+	title: "「禅ボディマインドマネジメント基礎」講座",
+	schedule: "13.14.15クール",
+	text: "2018年10月から12月にかけて3クール開講いたします。今回の秋クールは特典が多くなっております。是非この機会にご受講下さい。2018秋開講案内のお知らせの詳細はこちらにて。"
+};
+
+let item4 = {
+	link: "https://www.youtube.com/watch?v=0UK87DjFI6E",	
+	image: "./images/zen.png",
+	date: "2018.9.25",
+	category: "お知らせイベント案内",
+	title: "禅マインドフルネス身心堂zafu講義の",
+	schedule: "動画を公開しました",
+	text: "禅ボディマインドマネジメント講座の様子の動画を公開しました。"
 };
 
 let carousel = new Vue({
@@ -79,7 +89,7 @@ let carousel = new Vue({
 	data: {
 		navBtnLeft: "<img class=\"navigation-btn\" src=\"./images/btnLeft.png\">",
 		navBtnRight: "<img class=\"navigation-btn\" src=\"./images/btnRight.png\">",
-		items: [item1, item2, item3, item1, item2, item3]
+		items: [item1, item2, item3, item4]
 	}
 });
 let footer = new Vue({
